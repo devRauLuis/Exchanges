9594ee449c00361c1068ba1179f96db448aef647
Raul Luis <soyrauluis@gmail.com>
2022-06-28 23:52:14 -0400

    Add util

---
+++ app/src/main/java/com/devaruluis/exchanges/util.kt
@@ -0,0 +1,11 @@
+package com.devaruluis.exchanges
+
+import java.text.NumberFormat
+import java.util.*
+
+fun formatToCurrency(n: Float): String {
+    val format: NumberFormat = NumberFormat.getCurrencyInstance()
+    format.maximumFractionDigits = 2
+    format.currency = Currency.getInstance("USD")
+    return format.format(n)
+}
