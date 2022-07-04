b357d50d59fa8beb1a5d361af790cffa7c30d9c5
Raul Luis <soyrauluis@gmail.com>
2022-07-04 15:10:04 -0400

    Add Scaffold and navigation

--- app/src/main/java/com/devaruluis/exchanges/MainActivity.kt
+++ app/src/main/java/com/devaruluis/exchanges/MainActivity.kt
@@ -1,18 +1,25 @@
 package com.devaruluis.exchanges
 
+import Screen
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.currentBackStackEntryAsState
+import androidx.navigation.compose.rememberNavController
+import com.devaruluis.exchanges.ui.navigation.MainNavHost
 import com.devaruluis.exchanges.ui.theme.ExchangesTheme
-import com.devaruluis.exchanges.ui.view.ExchangesScreen
+import com.devaruluis.loanscompose.ui.Menu
+import com.devaruluis.loanscompose.ui.TopBar
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.launch
+
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
@@ -25,9 +32,57 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    ExchangesScreen()
+                    MainApp()
                 }
             }
         }
     }
 }
+
+@Composable
+fun MainApp() {
+    val allScreens = Screen.values().toList()
+    val navController = rememberNavController()
+    val backStackEntry = navController.currentBackStackEntryAsState()
+    var currentScreen = Screen.fromRoute(backStackEntry.value?.destination?.route)
+    val scaffoldState = rememberScaffoldState()
+    val scope = rememberCoroutineScope()
+    val snackbarHostState = remember { SnackbarHostState() }
+
+    val toggleDrawer = {
+        scope.launch {
+            scaffoldState.drawerState.apply {
+                if (isClosed) open() else close()
+            }
+        }
+    }
+
+    Scaffold(
+        scaffoldState = scaffoldState,
+        drawerContent = {
+            Menu(
+                allScreens = allScreens,
+                onTabSelected = { screen ->
+                    scope.launch {
+                        navController.navigate(screen.name)
+                        scaffoldState.drawerState.apply {
+                            close()
+                        }
+                    }
+                },
+                currentScreen = currentScreen, closeMenu = {
+                    toggleDrawer()
+                }
+            )
+        },
+        topBar = {
+            TopBar(
+                onMenuClick = { toggleDrawer() }
+            )
+        },
+    ) { innerPadding ->
+        MainNavHost(navController, modifier = Modifier.padding(innerPadding))
+    }
+    SnackbarHost(hostState = snackbarHostState)
+}
+
